a43599a0dbcf8b4f6e6a2c6daddae707bb86c104
jiyounglee <dlwldud913@naver.com>
2022-06-20 17:59:50 +0900

    2022/06/22

---
+++ src/main/java/baekjoon/level5/n_15596.java
@@ -0,0 +1,11 @@
+package baekjoon.level5;
+
+public class n_15596 {
+    public long sum(int [] a){
+        long result=0;
+        for(int i=0; i<a.length; i++){
+            result+=a[i];
+        }
+        return result;
+    }
+}
---
+++ src/main/java/baekjoon/level5/n_4673.java
@@ -0,0 +1,31 @@
+package baekjoon.level5;
+
+public class n_4673 {
+
+    public static void main(String[] args){
+
+        boolean[] arr = new boolean[10001];
+
+        for(int i=1; i<10001; i++){
+            int index= checkNum(i);
+
+            if(index<10001)
+                arr[index]=true;
+        }
+
+        for(int i=1; i<10001; i++){
+            if(!arr[i])
+                System.out.println(i);
+        }
+    }
+
+    public static int checkNum(int num){
+        int sum=num;
+
+        while(num!=0){
+            sum+=num%10;
+            num=num/10;
+        }
+        return sum;
+    }
+}
